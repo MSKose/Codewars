b00b2ccb5408b626a6501b0ec6d601cac776c28b
MSKose <saitmustafa1998@gmail.com>
2022-08-06 20:40:11 +0200

    added the 8 kyu Grasshopper - Combine strings

---
+++ 8 kyu/Grasshopper - Combine strings.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/55f73f66d160f1f1db000059/
+
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+*/
+
+// my solution
+const combineNames = (...fullname) => fullname.join(' ');
+// playin' with the spread operator. 
