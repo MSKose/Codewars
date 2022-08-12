a442f2d086505aefafeb77b503f835ed8303c28e
MSKose <saitmustafa1998@gmail.com>
2022-08-12 22:47:50 +0200

    added the 8 kyu Reversed Words

---
+++ 8 kyu/Reversed Words.js
@@ -0,0 +1,24 @@
+/*
+https://www.codewars.com/kata/51c8991dee245d7ddf00000e/
+
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example(Input --> Output):
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+*/
+
+// my solution
+function reverseWords(str) {
+  return str.split(" ").reverse().join` `;
+}
+
+//! alternative-solution
+function reverseWords(str) {
+  var reverse = [];
+  var words = str.split(" ");
+  for (let i = words.length - 1; i >= 0; i--) {
+    reverse.push(words[i]);
+  }
+  return reverse.join(" ");
+}
