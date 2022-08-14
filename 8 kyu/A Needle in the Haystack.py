138d11ac9fc2cc66f3b5fdd01f7a624ae4f1b5fa
MSKose <saitmustafa1998@gmail.com>
2022-08-14 21:03:28 +0200

    added the 8kyu A Needle in the Haystack

---
+++ 8 kyu/A Needle in the Haystack.py
@@ -0,0 +1,26 @@
+'''
+https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+'''
+
+# my solution
+def find_needle(haystack):
+    for idx, x in enumerate(haystack):
+        if x == "needle":
+            return f"found the needle at position {idx}"
+
+#! alternative-solution
+def find_needle(haystack):
+    return f'found the needle at position {haystack.index("needle")}'
+# so apparantly index() in python is similar to indexOf() method in js
