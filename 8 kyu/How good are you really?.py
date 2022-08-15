973b493bbdf5ae535f8ae51dbfa039afb2e6d496
MSKose <saitmustafa1998@gmail.com>
2022-08-15 23:50:07 +0200

    added the 8kyu How good are you really?

---
+++ 8 kyu/How good are you really?.py
@@ -0,0 +1,18 @@
+'''
+https://www.codewars.com/kata/5601409514fc93442500010b
+
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+'''
+
+# my solution
+def better_than_average(class_points, your_points):
+    return True if sum(class_points) / len(class_points) < your_points else False
+
