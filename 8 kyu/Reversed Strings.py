527a97bd5630b26feec2060310d10e39caa8d2ef
MSKose <saitmustafa1998@gmail.com>
2022-08-16 23:11:39 +0200

    added the 8kyu Reversed Strings

---
+++ 8 kyu/Reversed Strings.py
@@ -0,0 +1,12 @@
+'''
+https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+'''
+
+# my solution
+def solution(string):
+    return string[::-1]
