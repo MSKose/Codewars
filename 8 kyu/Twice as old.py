3c500630132241ba13e5e3479dad90638c0894ca
MSKose <saitmustafa1998@gmail.com>
2022-08-17 11:13:18 +0200

    added the 8kyu Twice as old

---
+++ 8 kyu/Twice as old.py
@@ -0,0 +1,13 @@
+'''
+https://www.codewars.com/kata/5b853229cfde412a470000d0/
+
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+'''
+
+# my solution
+def twice_as_old(dad, son):
+    return abs(dad - (2 * son))
