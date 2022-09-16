d9015459db466a25378c5b074d4a07d23e7327eb
MSKose <saitmustafa1998@gmail.com>
2022-09-16 23:29:19 +0200

    added the 6kyu Count the smiley faces!

---
+++ 6 kyu/Count the smiley faces!.py
@@ -0,0 +1,45 @@
+'''
+https://www.codewars.com/kata/583203e6eb35d7980400002a/
+
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+
+Example
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+Note
+In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+'''
+
+# my solution
+def count_smileys(arr):
+    valids = [':)', ':D', ':-)', ':-D', ':~)', ':-D', ';)', ';D', ';-)', ';-D', ';~)', ';~D']
+    
+    count = 0
+    
+    for i in arr:
+        if i in valids:
+            count += 1
+            
+    return count
+
+#! alternative-solution-1
+import re
+def count_smileys(arr):
+    return len(re.findall('[:;]{1}[-~]?[)D]{1}', str(arr)))
+
+#! alternative-solution-2
+valid = ":) :D :-) :-D :~) :~D ;) ;D ;-) ;-D ;~) ;~D".split()
+
+def count_smileys(arr):
+    return sum(face in valid for face in arr)
