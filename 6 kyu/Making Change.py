26fa503e8f27b4520b73f6c87008a180ba6c6003
MSKose <saitmustafa1998@gmail.com>
2022-09-26 14:52:12 +0200

    added the 6kyu Making Change

---
+++ 6 kyu/Making Change.py
@@ -0,0 +1,43 @@
+'''
+https://www.codewars.com/kata/5365bb5d5d0266cd010009be/
+
+Making Change
+Complete the method that will determine the minimum number of coins needed to make change for a given amount in American currency.
+
+Coins used will be half-dollars, quarters, dimes, nickels, and pennies, worth 50¢, 25¢, 10¢, 5¢ and 1¢, respectively. They'll be represented by the symbols H, Q, D, N and P (symbols in Ruby, strings in in other languages)
+
+The argument passed in will be an integer representing the value in cents. The return value should be a hash/dictionary/object with the symbols as keys, and the numbers of coins as values. Coins that are not used should not be included in the hash. If the argument passed in is 0, then the method should return an empty hash.
+
+Examples
+makeChange(0)   //-->  {}
+makeChange(1)   //-->  {"P":1}
+makeChange(43)  //-->  {"Q":1, "D":1, "N":1, "P":3}
+makeChange(91)  //-->  {"H":1, "Q":1, "D":1, "N":1, "P":1}
+'''
+
+# my solution
+def make_change(amount):
+    coins = { 'H': 50, 'Q': 25, 'D': 10, 'N': 5, 'P': 1 }
+    result = {}
+    
+    for key, value in coins.items():
+        newKey = (amount / value) // 1
+        
+        if (newKey != 0): 
+            result[key] = newKey
+            
+        amount = amount % value
+    
+    return result
+
+#! alternative-solution
+BASE = {"H": 50, "Q": 25, "D": 10, "N": 5, "P": 1}
+
+def make_change(n):
+    r = {}
+    for x, y in BASE.items():
+        if n >= y:
+            r[x], n = divmod(n, y)
+    return r
+
+# wow, didn't know about the divmod()
