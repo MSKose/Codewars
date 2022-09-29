8fe04f11052128aea5212ebade29894eb0948090
MSKose <saitmustafa1998@gmail.com>
2022-09-29 09:16:27 +0200

    added the 6kyu Sort the odd

---
+++ 6 kyu/Sort the odd.py
@@ -0,0 +1,15 @@
+'''
+https://www.codewars.com/kata/578aa45ee9fd15ff4600090d
+
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+'''
+
+# my solution
+def sort_array(arr):
+    new_arr = sorted([x for x in arr if x % 2 == 1], reverse=True)
+    return [new_arr.pop() if y % 2 else y for y in arr]
