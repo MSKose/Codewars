987d692cc4f4fe3b70b77ceea4e4dea8f8f5f8ec
MSKose <saitmustafa1998@gmail.com>
2022-10-25 09:14:50 +0200

    added the 7kyu Greet Me

---
+++ 7 kyu/Greet Me.py
@@ -0,0 +1,14 @@
+'''
+https://www.codewars.com/kata/535474308bb336c9980006f2
+
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+'''
+
+# my solution
+def greet(name): 
+    return "Hello " + name.capitalize() + "!"
