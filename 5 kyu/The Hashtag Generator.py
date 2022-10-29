1b2270614bb5ec7d384685c41827a5a588e21e20
MSKose <saitmustafa1998@gmail.com>
2022-10-29 17:50:06 +0200

    added the 5kyu The Hashtag Generator

---
+++ 5 kyu/The Hashtag Generator.py
@@ -0,0 +1,27 @@
+'''
+https://www.codewars.com/kata/52449b062fb80683ec000024
+
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+Examples
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+'''
+
+# my solution
+import re
+
+def generate_hashtag(s):
+    if not s: return False
+    x = re.sub(r"\b[a-zA-Z]+\b", lambda x: x.group(0).title(), s) 
+    y = re.sub(r"\s", '', x) 
+    result = '#' + y
+    return result if len(result) < 140 else False
