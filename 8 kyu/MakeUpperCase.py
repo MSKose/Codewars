5d6ef7dad0153fe94ea0fd9068e97ac9ad807f57
MSKose <saitmustafa1998@gmail.com>
2022-10-30 23:36:37 +0100

    added the 8kyu MakeUpperCase

---
+++ 8 kyu/MakeUpperCase.py
@@ -0,0 +1,9 @@
+'''
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+Write a function which converts the input string to uppercase.
+'''
+
+# my solution
+def make_upper_case(s):
+    return s.upper()
