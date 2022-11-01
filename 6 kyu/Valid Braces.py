4c6b4695b528894b493a19ee1ab6fe1d4738fcf8
MSKose <saitmustafa1998@gmail.com>
2022-11-01 09:43:53 +0100

    added the 6kyu Valid Braces

---
+++ 6 kyu/Valid Braces.py
@@ -0,0 +1,37 @@
+'''
+https://www.codewars.com/kata/5277c8a221e209d3f6000b56
+
+Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
+
+This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+
+All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+
+What is considered Valid?
+A string of braces is considered valid if all braces are matched with the correct brace.
+
+Examples
+"(){}[]"   =>  True
+"([{}])"   =>  True
+"(}"       =>  False
+"[(])"     =>  False
+"[({})](]" =>  False
+'''
+
+# my solution
+def valid_braces(s):
+    starting_braces = ['(', '[', '{']
+    closing_braces = [')', ']', '}']
+    braces = {'(':')', '[':']', '{':'}'}
+    arr = []
+    
+    for i in range(len(s)):
+        if s[i] in starting_braces:
+            arr.append(s[i])
+        else:
+            if len(arr) == 0 and s[i] in closing_braces:
+                return False
+            elif braces[arr[-1]] == s[i]:
+                arr.pop()
+            
+    return len(arr) == 0
