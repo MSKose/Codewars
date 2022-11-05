dba90e8427d64c437d2385acfa01c160310ab594
MSKose <saitmustafa1998@gmail.com>
2022-11-05 08:08:24 +0100

    added the 8kyu Third Angle of a Triangle

---
+++ 8 kyu/Third Angle of a Triangle.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/5a023c426975981341000014
+
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+*/
+
+// my solution
+const otherAngle = (a, b) => 180 - (a + b)
---
+++ 8 kyu/Third Angle of a Triangle.py
@@ -0,0 +1,13 @@
+'''
+https://www.codewars.com/kata/5a023c426975981341000014
+
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+'''
+
+# my solution
+def other_angle(a, b):
+    return 180 - (a + b)
