5e98851b2b752742c9eb2c40d43f6fda40615f7e
MSKose <saitmustafa1998@gmail.com>
2022-11-08 06:30:28 +0100

    added the 8kyu Century From Year

---
+++ 8 kyu/Century From Year.py
@@ -0,0 +1,26 @@
+'''
+https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+
+Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+'''
+
+# my solution
+from math import ceil
+
+def century(year):
+    return ceil(year / 100)
+
+#! alternative-solution
+def century(year):
+    return (year + 99) // 100
+# pure math, love it
