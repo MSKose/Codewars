53c971bd4c320c1d01400c51852c6fc1b036c110
MSKose <saitmustafa1998@gmail.com>
2022-11-14 08:52:01 +0100

    added the 7kyu Anagram Detection

---
+++ 7 kyu/Anagram Detection.py
@@ -0,0 +1,25 @@
+'''
+https://www.codewars.com/kata/529eef7a9194e0cbc1000255
+
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"
+'''
+
+# my solution
+def is_anagram(test, original):
+    return sorted(test.lower()) == sorted(original.lower())
+
+#! alternative-solution
+from collections import Counter
+
+def is_anagram(test, original):
+    return Counter(test.lower()) == Counter(original.lower())
+# this is faster than mine
