25017de128a02b079d8e00c6375146a41692e6af
MSKose <saitmustafa1998@gmail.com>
2022-11-17 08:58:07 +0100

    added the 8kyu Mr. Freeze

---
+++ 8 kyu/Mr. Freeze.js
@@ -0,0 +1,8 @@
+/*
+https://www.codewars.com/kata/514a3996d22ce03198000003
+
+There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+*/
+
+// my solution
+Object.freeze(MrFreeze);
