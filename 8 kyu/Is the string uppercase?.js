5285422a68d8309ddb6d2c004275be9fdec23666
MSKose <saitmustafa1998@gmail.com>
2022-11-22 23:56:48 +0100

    added the 8kyu Is the string uppercase?

---
+++ 8 kyu/Is the string uppercase?.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/56cd44e1aa4ac7879200010b
+
+Create a method to see whether the string is ALL CAPS.
+
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+*/
+
+// my solution
+String.prototype.isUpperCase = function (str) {
+  return String(this) === this.toUpperCase();
+};
