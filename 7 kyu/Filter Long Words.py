afac8aa2428b2022dd3e76d680ca3146e88703e2
MSKose <saitmustafa1998@gmail.com>
2022-11-23 10:06:47 +0100

    added the 7kyu Filter Long Words

---
+++ 7 kyu/Filter Long Words.py
@@ -0,0 +1,14 @@
+'''
+https://www.codewars.com/kata/5697fb83f41965761f000052
+
+Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+
+Example:
+
+* With input "The quick brown fox jumps over the lazy dog", 4
+* Return ['quick', 'brown', 'jumps']
+'''
+
+# my solution
+def filter_long_words(sentence, n):
+    return [word for word in sentence.split() if len(word) > n]
