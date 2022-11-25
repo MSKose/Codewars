49c52a2c40a40a1c16aa37910644d7499bbe7f51
MSKose <saitmustafa1998@gmail.com>
2022-11-25 10:47:09 +0100

    added the 7kyu Sum of angles

---
+++ 7 kyu/Sum of angles.py
@@ -0,0 +1,9 @@
+'''
+https://www.codewars.com/kata/5a03b3f6a1c9040084001765
+
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+'''
+
+# my solution
+def angle(n):
+    return (2 * n - 4) * 90
