d956f35cd0dcd175aef8b6473c9aebd7087efe3f
MSKose <saitmustafa1998@gmail.com>
2022-11-25 16:20:22 +0100

    added the 8kyu Convert to Binary

---
+++ 8 kyu/Convert to Binary.py
@@ -0,0 +1,20 @@
+'''
+Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+to_binary(1)  /* should return 1 */
+to_binary(5)  /* should return 101 */
+to_binary(11) /* should return 1011 */
+Example:
+
+toBinary(1)  /* should return 1 */
+toBinary(5)  /* should return 101 */
+toBinary(11) /* should return 1011 */
+'''
+
+# my solution
+def to_binary(n):
+    return int(bin(n)[2:])
+
+#! alternative-solution
+def to_binary(n):
+    return int(f'{n:b}')
