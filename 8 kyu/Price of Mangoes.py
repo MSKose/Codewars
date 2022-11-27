3523ba0fb9bd00008d2cd174f53fcc37306119c8
MSKose <saitmustafa1998@gmail.com>
2022-11-28 00:24:14 +0100

    added the 8kyu Price of Mangoes

---
+++ 8 kyu/Price of Mangoes.py
@@ -0,0 +1,14 @@
+'''
+https://www.codewars.com/kata/57a77726bb9944d000000b06
+
+
+There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+Examples
+mango(3, 3) ==> 6    # 2 mangoes for 3 = 6; +1 mango for free
+mango(9, 5) ==> 30   # 6 mangoes for 5 = 30; +3 mangoes for free
+'''
+
+# my solution
+def mango(quantity, price):
+    return (quantity - quantity // 3) * price
