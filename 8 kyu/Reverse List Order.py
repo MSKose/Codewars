6cb5b1833cda81e0a484817ad3763185f38f545a
MSKose <saitmustafa1998@gmail.com>
2022-11-28 00:06:59 +0100

    added the 8kyu Reverse List Order

---
+++ 8 kyu/Reverse List Order.py
@@ -0,0 +1,13 @@
+'''
+https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b
+
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+'''
+
+# my solution
+def reverse_list(l):
+    return l[::-1]
