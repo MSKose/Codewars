0eb0d3637be67152c36ddc7e4427210f8b24e2fe
MSKose <saitmustafa1998@gmail.com>
2022-11-27 19:00:26 +0100

    added the 8kyu Function 2 - squaring an argument

---
+++ 8 kyu/Function 2 - squaring an argument.py
@@ -0,0 +1,9 @@
+'''
+https://www.codewars.com/kata/523b623152af8a30c6000027
+
+Now you have to write a function that takes an argument and returns the square of it.
+'''
+
+# my solution
+def square(n):
+    return n * n
