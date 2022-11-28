1be279fe8dcac223980f867d6ed5cd80aae522f0
MSKose <saitmustafa1998@gmail.com>
2022-11-28 01:17:15 +0100

    added the 7kyu Sum of a sequence

---
+++ 7 kyu/Sum of a sequence.py
@@ -0,0 +1,20 @@
+'''
+https://www.codewars.com/kata/586f6741c66d18c22800010a
+
+Your task is to make function, which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+If begin value is greater than the end, function should returns 0
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)
+'''
+
+# my solution
+def sequence_sum(begin_number, end_number, step):
+    return sum(i for i in range(begin_number, end_number + 1, step))
