8b85f14f2bb4f7d19528748e2b6d89f04105205f
MSKose <saitmustafa1998@gmail.com>
2022-11-29 20:35:05 +0100

    added the 7kyu Find min and max

---
+++ 7 kyu/Find min and max.py
@@ -0,0 +1,9 @@
+'''
+https://www.codewars.com/kata/57a1ae8c7cb1f31e4e000130
+
+Implement a function that returns the minimal and the maximal value of a list (in this order).
+'''
+
+# my solution
+def get_min_max(seq): 
+    return (min(seq), max(seq))
