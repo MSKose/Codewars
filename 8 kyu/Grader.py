7fe67e6eb24f75780903be6b7d7502400d2e6f3b
MSKose <saitmustafa1998@gmail.com>
2022-12-05 22:49:00 +0100

    added the 8kyu Grader

---
+++ 8 kyu/Grader.py
@@ -0,0 +1,28 @@
+'''
+https://www.codewars.com/kata/53d16bd82578b1fb5b00128c
+
+Create a function that takes a number as an argument and returns a grade based on that number.
+
+Score	Grade
+Anything greater than 1 or less than 0.6	"F"
+0.9 or greater	"A"
+0.8 or greater	"B"
+0.7 or greater	"C"
+0.6 or greater	"D"
+Examples:
+
+grader(0)   should be "F"
+grader(1.1) should be "F"
+grader(0.9) should be "A"
+grader(0.8) should be "B"
+grader(0.7) should be "C"
+grader(0.6) should be "D"
+'''
+
+# my solution
+def grader(score):
+    if score >= 0.9 and score <= 1: return "A"
+    elif 0.8 <= score < 0.9 : return "B"
+    elif 0.7 <= score < 0.8 : return "C"
+    elif 0.6 <= score < 0.7 : return "D"
+    else: return "F"
