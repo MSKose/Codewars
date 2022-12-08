f939cc521f3485e68972850dfd11a2d4bd2add01
MSKose <saitmustafa1998@gmail.com>
2022-12-08 23:44:23 +0100

    added the 7kyu Sum even numbers

---
+++ 7 kyu/Sum even numbers.py
@@ -0,0 +1,17 @@
+'''
+https://www.codewars.com/kata/586beb5ba44cfc44ed0006c3
+
+Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
+
+Only numbers without decimals like 4 or 4.0 can be even.
+
+The input is a sequence of numbers: integers and/or floats.
+
+Examples
+[4, 3, 1, 2, 5, 10, 6, 7, 9, 8]  -->  30   # because 4 + 2 + 10 + 6 + 8 = 30
+[]                               -->  0
+'''
+
+# my solution
+def sum_even_numbers(seq): 
+    return sum(i for i in seq if not i % 2)
