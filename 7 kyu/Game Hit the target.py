aefec31e174a4ba50b761d91ce2b2e7188d98c50
MSKose <saitmustafa1998@gmail.com>
2022-12-09 20:48:17 +0100

    added the 7kyu Game Hit the target

---
+++ 7 kyu/Game Hit the target.py
@@ -0,0 +1,56 @@
+'''
+https://www.codewars.com/kata/5ffc226ce1666a002bf023d2
+
+Hit the target
+given a matrix n x n (2-7), determine if the arrow is directed to the target (x).
+There will be only 1 arrow '>' and 1 target 'x'
+An empty spot will be denoted by a space " ", the target with a cross "x", and the scope ">"
+Examples:
+given matrix 4x4:
+[
+
+  [' ', ' ', ' ', ' '],
+
+  [' ', ' ', ' ', ' '], --> return true
+
+  [' ', '>', ' ', 'x'],
+
+  [' ', ' ', ' ', ' ']
+
+] 
+given matrix 4x4:
+[
+
+  [' ', ' ', ' ', ' '],
+
+  [' ', '>', ' ', ' '], --> return false
+
+  [' ', ' ', ' ', 'x'],
+
+  [' ', ' ', ' ', ' ']
+
+] 
+given matrix 4x4:
+[
+
+  [' ', ' ', ' ', ' '],
+
+  [' ', 'x', '>', ' '], --> return false
+
+  [' ', '', ' ', ' '],
+
+  [' ', ' ', ' ', ' ']
+
+] 
+
+In this example, only a 4x4 matrix was used, the problem will have matrices of dimensions from 2 to 7
+Happy hacking as they say!
+'''
+
+# my solution
+def solution(mtrx):
+    for i in mtrx:
+        if '>' in i and 'x' in i:
+            if i.index('>') < i.index('x'):
+                return True
+    return False
