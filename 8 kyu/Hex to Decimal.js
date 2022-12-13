c594d73848e679b2345e8be0a95aa191256ed2a6
MSKose <saitmustafa1998@gmail.com>
2022-12-13 23:56:01 +0100

    added the 8kyu Hex to Decimal

---
+++ 8 kyu/Hex to Decimal.js
@@ -0,0 +1,8 @@
+/*
+https://www.codewars.com/kata/57a4d500e298a7952100035d
+
+Complete the function which converts hex number (given as a string) to a decimal number.
+*/
+
+// my solution
+const hexToDec = (hexString) => parseInt(hexString, 16);
---
+++ 8 kyu/Hex to Decimal.py
@@ -0,0 +1,9 @@
+'''
+https://www.codewars.com/kata/57a4d500e298a7952100035d
+
+Complete the function which converts hex number (given as a string) to a decimal number.
+'''
+
+# my solution
+def hex_to_dec(s):
+    return int(s, 16)
