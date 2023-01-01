69b31740ef4addadd9ae7fc13ca8d74467aa7daf
MSKose <saitmustafa1998@gmail.com>
2023-01-01 15:14:38 +0100

    added the 7kyu Dot Calculator

---
+++ 7 kyu/Dot Calculator.js
@@ -0,0 +1,38 @@
+/*
+https://www.codewars.com/kata/6071ef9cbe6ec400228d9531
+
+You have to write a calculator that receives strings for input. The dots will represent the number in the equation. There will be dots on one side, an operator, and dots again after the operator. The dots and the operator will be separated by one space.
+
+Here are the following valid operators :
+
++ Addition
+- Subtraction
+* Multiplication
+// Integer Division
+Your Work (Task)
+You'll have to return a string that contains dots, as many the equation returns. If the result is 0, return the empty string. When it comes to subtraction, the first number will always be greater than or equal to the second number.
+
+Examples (Input => Output)
+* "..... + ..............." => "...................."
+* "..... - ..."             => ".."
+* "..... - ."               => "...."
+* "..... * ..."             => "..............."
+* "..... * .."              => ".........."
+* "..... // .."             => ".."
+* "..... // ."              => "....."
+* ". // .."                 => ""
+* ".. - .."                 => ""
+*/
+
+// my solution
+function dotCalculator (equation) {
+    const spl = equation.split(' ');
+    const x = spl[0].length;
+    const y = spl[2].length;
+    const op = spl[1];
+    
+    if (op === '+') {return '.'.repeat(x + y)}
+    else if (op === '-') {return '.'.repeat(x - y)}
+    else if (op === '//') {return '.'.repeat(~~(x / y))}
+    else if (op === '*') {return '.'.repeat(x * y)}
+}
