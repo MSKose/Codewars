3a7cd60f36caa587409309a4ad8f1af37548fdc9
MSKose <saitmustafa1998@gmail.com>
2023-01-03 17:24:49 +0100

    added the 7kyu Dictionary from two lists

---
+++ 7 kyu/Dictionary from two lists.js
@@ -0,0 +1,34 @@
+/*
+https://www.codewars.com/kata/5533c2a50c4fea6832000101
+
+There are two lists, possibly of different lengths. The first one consists of keys, the second one consists of values. Write a function createDict(keys, values) that returns a dictionary created from keys and values. If there are not enough values, the rest of keys should have a None (JS null)value. If there not enough keys, just ignore the rest of values.
+
+Example 1:
+
+keys = ['a', 'b', 'c', 'd']
+values = [1, 2, 3]
+createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3, 'd': null}
+Example 2:
+
+keys = ['a', 'b', 'c']
+values = [1, 2, 3, 4]
+createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3}
+*/
+
+// my solution
+function createDict(keys, values) {
+  let dict = {};
+
+  for (let i in keys) {
+    dict[keys[i]] = values.length <= i ? null : values[i];
+  }
+
+  return dict;
+}
+
+//! alternative-solution
+function createDict(keys, values){
+    return keys.reduce(function(o, v, i){
+      return o[v] = i in values ? values[i] : null, o;
+    }, {});
+}
