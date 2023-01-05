38fad5417dd5bb24a7459e75bfb9838a60cf0be7
MSKose <saitmustafa1998@gmail.com>
2023-01-05 22:09:18 +0100

    added the 7kyu MinMinMax

---
+++ 7 kyu/MinMinMax.py
@@ -0,0 +1,31 @@
+'''
+https://www.codewars.com/kata/58d3487a643a3f6aa20000ff
+
+Given an unsorted array of integers, find the smallest number in the array, the largest number in the array, and the smallest number between the two array bounds that is not in the array.
+
+For instance, given the array [-1, 4, 5, -23, 24], the smallest number is -23, the largest number is 24, and the smallest number between the array bounds is -22. You may assume the input is well-formed.
+
+You solution should return an array [smallest, minimumAbsent, largest]
+
+The smallest integer should be the integer from the array with the lowest value.
+
+The largest integer should be the integer from the array with the highest value.
+
+The minimumAbsent is the smallest number between the largest and the smallest number that is not in the array.
+
+minMinMax([-1, 4, 5, -23, 24]); //[-23, -22, 24]
+minMinMax([1, 3, -3, -2, 8, -1]); //[-3, 0, 8]
+minMinMax([2, -4, 8, -5, 9, 7]); //[-5, -3,9]
+'''
+
+# my solution
+def min_min_max(arr):
+    mini = min(arr)
+    maxi = max(arr)
+    mid = lambda x: mid(x + 1) if x in arr else x
+    return [mini, mid(mini + 1), maxi]
+
+#! alternative-solution
+def minMinMax(arr):
+    s, mi, ma = set(arr), min(arr), max(arr)
+    return [mi, next(x for x in range(mi+1, ma) if x not in s), ma]
