ecf842e6dce2d1c2e9364fd427e7d2f86bf48b64
MSKose <saitmustafa1998@gmail.com>
2023-01-09 22:28:53 +0100

    added the 7kyu Factorial Factory

---
+++ 7 kyu/Factorial Factory.py
@@ -0,0 +1,21 @@
+'''
+https://www.codewars.com/kata/528e95af53dcdb40b5000171
+
+In mathematics, the factorial of integer 'n' is written as 'n!'. It is equal to the product of n and every integer preceding it. For example: 5! = 1 x 2 x 3 x 4 x 5 = 120
+
+Your mission is simple: write a function that takes an integer 'n' and returns 'n!'.
+
+You are guaranteed an integer argument. For any values outside the positive range, return null, nil or None .
+
+Note: 0! is always equal to 1. Negative values should return null;
+
+For more on Factorials : http://en.wikipedia.org/wiki/Factorial
+'''
+
+# my solution
+def factorial(n):
+    if n < 0: 
+        return None
+    if n <= 1:
+        return 1
+    return n * factorial(n - 1)
