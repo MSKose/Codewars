6101887565ed58c2aeeadb456226ce9e82cbecfa
MSKose <saitmustafa1998@gmail.com>
2023-01-14 23:52:19 +0100

    added the 7kyu How many arguments

---
+++ 7 kyu/How many arguments.js
@@ -0,0 +1,11 @@
+/*
+https://www.codewars.com/kata/5c44b0b200ce187106452139
+
+Create a function args_count, that returns the count of passed arguments
+
+args_count(1, 2, 3) -> 3
+args_count(1, 2, 3, 10) -> 4
+*/
+
+// my solution
+const args_count = (...n) => n.length;
