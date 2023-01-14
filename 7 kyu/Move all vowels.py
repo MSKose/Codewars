d17d9efd5131e412476489446a2ac8c10211fd99
MSKose <saitmustafa1998@gmail.com>
2023-01-14 23:40:46 +0100

    added the 7kyu Move all vowels

---
+++ 7 kyu/Move all vowels.py
@@ -0,0 +1,19 @@
+'''
+https://www.codewars.com/kata/56bf3287b5106eb10f000899
+
+Given a string as input, move all of its vowels to the end of the string, in the same order as they were before.
+
+Vowels are (in this kata): a, e, i, o, u
+
+Note: all provided input strings are lowercase.
+
+Examples
+"day"    ==>  "dya"
+"apple"  ==>  "pplae"
+'''
+
+# my solution
+import re
+
+def move_vowels(input): 
+    return re.sub('[aeiou]', '', input) + re.sub('[^aeiou]', '', input)
