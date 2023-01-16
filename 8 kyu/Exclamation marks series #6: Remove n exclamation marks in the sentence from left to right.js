f5acf372b0fdbfd6f4016af98a23de797ccc55ff
MSKose <saitmustafa1998@gmail.com>
2023-01-16 19:53:25 +0100

    added the 8kyu Exclamation marks series #6: Rem...

---
+++ 8 kyu/Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right.js
@@ -0,0 +1,32 @@
+/*
+https://www.codewars.com/kata/57faf7275c991027af000679
+
+Description:
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+Examples
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+*/
+
+// my solution
+function remove(s, n){
+    let replacedCount = 0;
+    return s.replace(/(!)/g, (match) => {
+      if (replacedCount < n) {
+        replacedCount++;
+        return "";
+      } else {
+        return match;
+      }
+    })
+}
