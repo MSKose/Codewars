562f5aa832595b8918cd34cb15b579d901a4323d
MSKose <saitmustafa1998@gmail.com>
2023-01-18 22:25:29 +0100

    added the 6kyu Consecutive strings

---
+++ 6 kyu/Consecutive strings.py
@@ -0,0 +1,41 @@
+'''
+https://www.codewars.com/kata/56a5d994ac971f1ac500003e
+
+You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Examples:
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+
+In the same way:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+
+Note
+consecutive strings : follow one after another without an interruption
+'''
+
+# my solution
+def longest_consec(strarr, k):
+    leng = len(strarr)
+    if leng == 0 or k > leng or k <= 0:
+        return ''
+    
+    longest_string = ''
+    for i in range(leng):
+        new_string = ''.join(strarr[i : i + k])
+        if len(new_string) > len(longest_string):
+            longest_string = new_string
+            
+    return longest_string
