c568bfd3c054c9bbf26336471b743c911fedea0d
MSKose <saitmustafa1998@gmail.com>
2023-01-28 21:44:37 +0100

    added the 7kyu Coding Meetup #3 - Higher-Order...

---
+++ 7 kyu/Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming?.js
@@ -0,0 +1,26 @@
+/*
+https://www.codewars.com/kata/5827acd5f524dd029d0005a4
+
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if at least one Ruby developer has signed up; or
+false if there will be no Ruby developers.
+For example, given the following input array:
+
+list1 = [
+    { 'firstName': 'Sofia', 'lastName': 'I.', 'country': 'Argentina', 'continent': 'Americas', 'age': 35, 'language': 'Java' },
+    { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 35, 'language': 'Python' },
+    { 'firstName': 'Madison', 'lastName': 'U.', 'country': 'United States', 'continent': 'Americas', 'age': 32, 'language': 'Ruby' } 
+    ]
+your function should return true.
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+*/
+
+// my solution
+const isRubyComing = list =>
+  list.some(x => x['language'] === 'Ruby');
