89be2637f1a6443543c1b67ce5749112546f5398
MSKose <saitmustafa1998@gmail.com>
2023-02-04 20:07:13 +0100

    added the 7kyu The old switcheroo 2

---
+++ 7 kyu/The old switcheroo 2.py
@@ -0,0 +1,25 @@
+'''
+https://www.codewars.com/kata/55d6a0e4ededb894be000005
+
+https://www.codewars.com/kata/55d6a0e4ededb894be000005
+
+This is a follow up from my kata The old switcheroo
+
+Write the function :
+
+function encode(str)
+that takes in a string str and replaces all the letters with their respective positions in the English alphabet.
+
+encode('abc') == '123'   // a is 1st in English alpabet, b is 2nd and c is 3rd
+encode('codewars') == '315452311819'
+encode('abc-#@5') == '123-#@5'
+String are case sensitive.
+
+// Bonus point if you don't use toLowerCase()
+'''
+
+# my solution
+import re
+
+def encode(string):
+    return re.sub('[a-z]', lambda x: str(ord(x.group()) - 96), string.lower())
