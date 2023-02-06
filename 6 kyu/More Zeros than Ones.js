132372cc6f12a56f5d59a6d97596c7ad6f3e32d1
MSKose <saitmustafa1998@gmail.com>
2023-02-06 23:20:56 +0100

    added the 6kyu More Zeros than Ones

---
+++ 6 kyu/More Zeros than Ones.js
@@ -0,0 +1,26 @@
+/*
+https://www.codewars.com/kata/5d41e16d8bad42002208fe1a
+
+Create a moreZeros function which will receive a string for input, and return an array (or null terminated string in C) containing only the characters from that string whose binary representation of its ASCII value consists of more zeros than ones.
+
+You should remove any duplicate characters, keeping the first occurrence of any such duplicates, so they are in the same order in the final array as they first appeared in the input string.
+
+Examples
+
+'abcde' === ["1100001", "1100010", "1100011", "1100100", "1100101"]
+               True       True       False      True       False
+                   
+        --> ['a','b','d']
+    
+'DIGEST'--> ['D','I','E','T']
+All input will be valid strings of length > 0. Leading zeros in binary should not be counted.
+*/
+
+// my solution
+function moreZeros(s){
+    return [...new Set([...s].filter(x => {
+      const a = x.charCodeAt().toString(2);
+      const zero = a.split('0').length - 1;
+      return zero > (a.length - zero);
+    }))];
+}
