f5aa81d7a3ed0512722c95bba3b73cfc25d032ec
MSKose <saitmustafa1998@gmail.com>
2023-02-11 00:12:49 +0100

    added the 7kyu Turn with a Compass

---
+++ 7 kyu/Turn with a Compass.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/61a8c3a9e5a7b9004a48ccc2
+
+You have an 8-wind compass, like this:
+
+You receive the direction you are facing (one of the 8 directions: N, NE, E, SE, S, SW, W, NW) and a certain degree to turn (a multiple of 45, between -1080 and 1080); positive means clockwise, and negative means counter-clockwise.
+
+Return the direction you will face after the turn.
+
+Examples
+"S",  180  -->  "N"
+"SE", -45  -->  "E"
+"W",  495  -->  "NE"
+*/
+
+// my solution
+const DIRECTIONS = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+const direction = (facing, turn) =>
+  DIRECTIONS[(((DIRECTIONS.indexOf(facing) + ~~(turn / 45)) % 8) + 8) % 8];
