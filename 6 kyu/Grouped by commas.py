4b6f13a14b7e9ab6113eb36109bbf2638a429e63
MSKose <saitmustafa1998@gmail.com>
2023-03-14 22:52:29 +0100

    added the 6 kyu Grouped by commas

---
+++ 6 kyu/Grouped by commas.py
@@ -0,0 +1,33 @@
+'''
+https://www.codewars.com/kata/5274e122fc75c0943d000148
+
+Finish the solution so that it takes an input n (integer) and returns a string that is the decimal representation of the number grouped by commas after every 3 digits.
+
+Assume: 0 <= n < 2147483647
+
+Examples
+       1  ->           "1"
+      10  ->          "10"
+     100  ->         "100"
+    1000  ->       "1,000"
+   10000  ->      "10,000"
+  100000  ->     "100,000"
+ 1000000  ->   "1,000,000"
+35235235  ->  "35,235,235"
+'''
+
+# my solution
+def group_by_commas(n):
+    count = 0
+    result = ''
+    num = str(n)
+    
+    for i in range(len(num) - 1, -1, -1):
+        if count == 3:
+            count = 0
+            result = num[i] + ',' + result
+        else:
+            result = num[i] + result
+        count += 1
+            
+    return result
