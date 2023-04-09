16b9af4dab0db316a32d3fc546a466538f348ba9
MSKose <saitmustafa1998@gmail.com>
2023-04-09 13:50:49 +0200

    added the 6kyu How many pages in a book?

---
+++ 6 kyu/How many pages in a book?.js
@@ -0,0 +1,28 @@
+/*
+https://www.codewars.com/kata/622de76d28bf330057cd6af8
+
+Every book has n pages with page numbers 1 to n. The summary is made by adding up the number of digits of all page numbers.
+
+Task: Given the summary, find the number of pages n the book has.
+
+Example
+If the input is summary=25, then the output must be n=17: The numbers 1 to 17 have 25 digits in total: 1234567891011121314151617.
+
+Be aware that you'll get enormous books having up to 100.000 pages.
+
+All inputs will be valid.
+
+Here's the opposite kata, which is more complex (Paginating a huge book)
+*/
+
+// my solution
+function amountOfPages(summary) {
+  let n = 1;
+  let lenCount = String(n).length;
+
+  while (lenCount < summary) {
+    lenCount += String(++n).length;
+  }
+
+  return n;
+}
