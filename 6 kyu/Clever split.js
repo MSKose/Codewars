ec9c7bc6edd02d55d00090f1d809d87bf5c916f8
MSKose <saitmustafa1998@gmail.com>
2023-04-23 09:00:02 +0200

    added the 6kyu Clever split

---
+++ 6 kyu/Clever split.js
@@ -0,0 +1,22 @@
+/*
+https://www.codewars.com/kata/5992e11d6ca73b38d50000f0
+
+You have a string. The string consists of words. Before words can be an exclamation mark !. Also some words are marked as one set with square brackets []. You task is to split the string into separate words and sets.
+ The set can't be contained in another set.
+
+Input:
+ String with words (separated by spaces), ! and [].
+
+Output:
+ Array with separated words and sets.
+
+Examples:
+
+('Buy a !car [!red green !white] [cheap or !new]')  =>  ['Buy', 'a', '!car', '[!red green !white]', '[cheap or !new]']
+('!Learning !javascript is [a joy]')                =>  ['!Learning', '!javascript', 'is', '[a joy]']
+('[Cats and dogs] are !beautiful and [cute]')       =>  ['[Cats and dogs]', 'are', '!beautiful', 'and', '[cute]']
+*/
+
+// my solution
+const cleverSplit = (s) =>
+  s.match(/\[[^\]]*\]|\S+/g)
