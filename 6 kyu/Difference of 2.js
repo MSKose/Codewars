372e2ae425bc914a17365b6eaf31edae32f6ff1c
MSKose <saitmustafa1998@gmail.com>
2023-04-29 21:12:42 +0200

    added the 6kyu Difference of 2

---
+++ 6 kyu/Difference of 2.js
@@ -0,0 +1,34 @@
+/*
+https://www.codewars.com/kata/5340298112fa30e786000688
+
+The objective is to return all pairs of integers from a given array of integers that have a difference of 2.
+
+The result array should be sorted in ascending order of values.
+
+Assume there are no duplicate integers in the array. The order of the integers in the input array should not matter.
+
+Examples
+[1, 2, 3, 4]  should return [[1, 3], [2, 4]]
+
+[4, 1, 2, 3]  should also return [[1, 3], [2, 4]]
+
+[1, 23, 3, 4, 7] should return [[1, 3]]
+
+[4, 3, 1, 5, 6] should return [[1, 3], [3, 5], [4, 6]]
+*/
+
+// my solution
+function twosDifference(input) {
+  let sorted = input.sort((a, b) => a - b);
+  let res = [];
+
+  for (let i = 0; i < sorted.length; i++) {
+    for (let k = 0; k < sorted.length; k++) {
+      if (sorted[k] - sorted[i] === 2) {
+        res.push([sorted[i], sorted[k]]);
+      }
+    }
+  }
+
+  return res;
+}
