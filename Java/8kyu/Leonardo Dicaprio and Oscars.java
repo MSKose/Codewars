5731a0277df1f33ce6045d09c130026430b8edb0
MSKose <saitmustafa1998@gmail.com>
2023-05-30 21:21:37 +0200

    added the 8kyu Leonardo Dicaprio and Oscars

---
+++ Java/8kyu/Leonardo Dicaprio and Oscars.java
@@ -0,0 +1,23 @@
+/*
+https://www.codewars.com/kata/56d49587df52101de70011e4
+
+You have to write a function that describe Leo:
+
+function leo(oscar) {
+
+}
+if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
+if it was over 88 you should return "Leo got one already!"
+*/
+
+// my solution
+public class Kata {
+    public static String leo(final int oscar) {
+      return oscar > 88 ? "Leo got one already!"
+        : oscar == 86 ? "Not even for Wolf of wallstreet?!"
+        : oscar < 88 ? "When will you give Leo an Oscar?"
+        : "Leo finally won the oscar! Leo is happy";
+    }
+}
