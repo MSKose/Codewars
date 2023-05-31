14930676e1cb1109b2764ac1c3a26821a7062f14
MSKose <saitmustafa1998@gmail.com>
2023-05-31 21:49:05 +0200

    added the 8kyu MakeUpperCase

---
+++ Java/8kyu/MakeUpperCase.java
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+Write a function which converts the input string to uppercase.
+*/
+
+// my solution
+class Upper{
+    public static String MakeUpperCase(String str){
+      return str.toUpperCase();
+    }
+}
