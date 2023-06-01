7c57773e5244dbaf425c5e03695481ad1e3151a4
MSKose <saitmustafa1998@gmail.com>
2023-06-02 00:37:10 +0200

    added the 8kyu No zeros for heros

---
+++ Java/8kyu/No zeros for heros.java
@@ -0,0 +1,24 @@
+/*
+https://www.codewars.com/kata/570a6a46455d08ff8d001002
+
+Numbers ending with zeros are boring.
+
+They might be fun in your world, but not here.
+
+Get rid of them. Only the ending ones.
+
+1450 -> 145
+960000 -> 96
+1050 -> 105
+-1050 -> -105
+Zero alone is fine, don't worry about it. Poor guy anyway
+*/
+
+// my solution
+public class NoBoring {
+    public static int noBoringZeros(int n) {
+        return n == 0 
+          ? 0
+          : Integer.parseInt((n + "").replaceAll("0+$", ""));
+    }
+}
