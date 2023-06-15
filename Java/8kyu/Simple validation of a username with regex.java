ceb69c6d76e4deb38910a70902b9d6575a27052e
MSKose <saitmustafa1998@gmail.com>
2023-06-15 22:10:54 +0200

    added the 8kyu Simple validation of a username...

---
+++ Java/8kyu/Simple validation of a username with regex.java
@@ -0,0 +1,17 @@
+/*
+https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023
+
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+*/
+
+// my solution
+public class ZywOo {
+    public static boolean validateUsr(String s) {
+      return s.matches("^[a-z0-9_]{4,16}$");
+    }
+}
