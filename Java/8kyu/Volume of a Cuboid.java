59fe653ab9d032493d9bb8aea06560ae1e949b2f
MSKose <saitmustafa1998@gmail.com>
2023-06-28 23:19:16 +0200

    added the 8kyu Volume of a Cuboid

---
+++ Java/8kyu/Volume of a Cuboid.java
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+*/
+
+// my solution
+public class Kata {
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+      return length * width * height;
+    }
+}
