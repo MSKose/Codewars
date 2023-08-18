62f1205f6704ce9a5ebfe37c1c2ec42622ac4415
MSKose <saitmustafa1998@gmail.com>
2023-08-18 22:20:41 +0200

    added the 7kyu Last Survivor

---
+++ Java/7kyu/Last Survivor.java
@@ -0,0 +1,32 @@
+/*
+https://www.codewars.com/kata/609eee71109f860006c377d1
+
+DESCRIPTION:
+You are given a string of letters and an array of numbers.
+The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+After each removal the size of the string decreases (there is no empty space).
+Return the only letter left.
+
+Example:
+
+let str = "zbk", arr = [0, 1]
+    str = "bk", arr = [1]
+    str = "b", arr = []
+    return 'b'
+Notes
+The given string will never be empty.
+The length of the array is always one less than the length of the string.
+All numbers are valid.
+There can be duplicate letters and numbers.
+*/
+
+// my solution
+public class Kata {
+    public static String lastSurvivor(String letters, int[] coords) {
+        for (int i : coords) {
+            letters = letters.substring(0, i) + letters.substring(i + 1);
+        }
+
+        return letters;
+    }
+}
