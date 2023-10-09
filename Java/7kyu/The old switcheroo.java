c02c35b759021887e8278e617010619099c839ff
MSKose <saitmustafa1998@gmail.com>
2023-10-09 21:30:35 +0200

    added the 7kyu The old switcheroo

---
+++ Java/7kyu/The old switcheroo.java
@@ -0,0 +1,31 @@
+/*
+https://www.codewars.com/kata/55d410c492e6ed767000004f
+
+Write a function
+
+vowel2index(str)
+that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+E.g:
+
+vowel2index('this is my string') == 'th3s 6s my str15ng'
+vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+vowel2index('') == ''
+Your function should be case insensitive to the vowels.
+*/
+
+// my solution
+public class Kata {
+    public static String vowel2Index(String s) {
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if ("aeiouAEIOU".contains(Character.toString(s.charAt(i)))) {
+                str.append(i + 1);
+            } else {
+                str.append("" + s.charAt(i));
+            }
+        }
+
+        return str.toString();
+    }
+}
