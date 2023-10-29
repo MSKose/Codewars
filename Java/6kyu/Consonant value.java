2bf7fc057bb384d4f9ce4864fd71a0694e882444
MSKose <saitmustafa1998@gmail.com>
2023-10-29 10:38:09 +0100

    added the 6kyu Consonant value

---
+++ Java/6kyu/Consonant value.java
@@ -0,0 +1,33 @@
+/*
+https://www.codewars.com/kata/59c633e7dcc4053512000073/
+
+Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings. Consonants are any letters of the alphabet except "aeiou".
+
+We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+
+For example, for the word "zodiacs", let's cross out the vowels. We get: "z o d ia cs"
+
+-- The consonant substrings are: "z", "d" and "cs" and the values are z = 26, d = 4 and cs = 3 + 19 = 22. The highest is 26.
+solve("zodiacs") = 26
+
+For the word "strength", solve("strength") = 57
+-- The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. The highest is 57.
+*/
+
+// my solution
+public class ConsonantValue {
+    public static int solve(final String s) {
+        String[] arr = s.split("[aeiou]");
+        int maxLen = 0;
+        int temp = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!arr[i].isEmpty()) {  
+                temp = (int) arr[i].chars().map(x -> x - 96).sum();
+                maxLen = temp > maxLen ? temp : maxLen;
+            }
+        }
+
+        return maxLen;
+    }
+}
