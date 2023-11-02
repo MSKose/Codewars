75ee85a77e09944c85b562009189626fdfa56586
MSKose <saitmustafa1998@gmail.com>
2023-11-02 18:32:59 +0100

    added the 7kyu Counting Duplicates

---
+++ Java/6kyu/Counting Duplicates.java
@@ -0,0 +1,35 @@
+/*
+https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/
+
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+*/
+
+// my solution
+import java.util.HashSet;
+import java.util.Set;
+
+public class CountingDuplicates {
+    public static int duplicateCount(String text) {
+        Set<Character> setOfDuplicates = new HashSet<>();
+        Set<Character> setOfUniques = new HashSet<>();
+
+        for (char c : text.toLowerCase().toCharArray()) {
+            if (!setOfUniques.add(c)){
+                setOfDuplicates.add(c);
+                setOfUniques.remove(i);
+            }
+        }
+
+        return setOfDuplicates.size();
+    }
+}
