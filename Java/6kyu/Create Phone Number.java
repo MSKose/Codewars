34b1d61f70267c021a812d5864d83124ac6bf441
MSKose <saitmustafa1998@gmail.com>
2023-11-03 14:16:45 +0100

    added the 6kyu Create Phone Number

---
+++ Java/6kyu/Create Phone Number.java
@@ -0,0 +1,31 @@
+/*
+https://www.codewars.com/kata/525f50e3b73515a6db000b83
+
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+*/
+
+// my solution
+public class Kata {
+    public static String createPhoneNumber(int[] numbers) {
+        StringBuilder str = new StringBuilder("(***) ***-****");
+        
+        for (int i = 0; i < numbers.length; i++) { 
+            int idx = str.indexOf("*");
+            str.replace(idx, idx + 1, Integer.toString(numbers[i]));
+        }
+        
+        return str.toString();
+    }
+}
+
+//! alternative-solution
+public class Kata {
+    public static String createPhoneNumber(int[] numbers) {
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d", java.util.stream.IntStream.of(numbers).boxed().toArray());
+    }
+}
