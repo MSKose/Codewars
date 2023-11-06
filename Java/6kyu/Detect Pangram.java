ea227b4b37d9e0a4c7502599838e98d0a4c5f5a7
MSKose <saitmustafa1998@gmail.com>
2023-11-06 22:18:38 +0100

    added the 6kyu Detect Pangram

---
+++ Java/6kyu/Detect Pangram.java
@@ -0,0 +1,18 @@
+/*
+https://www.codewars.com/kata/545cedaa9943f7fe7b000048
+
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+*/
+
+// my solution
+import java.util.Arrays;
+
+public class PangramChecker {
+    public boolean check(String sentence){
+        return Arrays.stream(sentence.toLowerCase().replaceAll("[^a-z]", "").split(""))
+                    .distinct()
+                    .count() == 26;
+    }
+}
