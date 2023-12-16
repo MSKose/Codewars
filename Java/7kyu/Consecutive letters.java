f96d971ea94d4a8d3840caea394de5db1f0a5055
MSKose <saitmustafa1998@gmail.com>
2023-12-16 23:54:23 +0100

    added the 7kyu Consecutive letters

---
+++ Java/7kyu/Consecutive letters.java
@@ -0,0 +1,36 @@
+'''
+https://www.codewars.com/kata/5ce6728c939bf80029988b57
+
+In this Kata, we will check if a string contains consecutive letters as they appear in the English alphabet and if each letter occurs only once.
+
+Rules are: (1) the letters are adjacent in the English alphabet, and (2) each letter occurs only once.
+
+For example: 
+solve("abc") = True, because it contains a,b,c
+solve("abd") = False, because a, b, d are not consecutive/adjacent in the alphabet, and c is missing.
+solve("dabc") = True, because it contains a, b, c, d
+solve("abbc") = False, because b does not occur once.
+solve("v") = True
+All inputs will be lowercase letters.
+
+More examples in test cases. Good luck!
+'''
+
+// my solution
+import java.util.Arrays;
+
+class Solution{
+    public static boolean solve(String s){
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        String sorted = new String(chars);
+
+        for (int i = 0; i < sorted.length() - 1; i++) {
+            if (sorted.charAt(i) == sorted.charAt(i + 1) || sorted.charAt(i) + 1 != sorted.charAt(i + 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
