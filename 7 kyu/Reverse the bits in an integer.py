e241256c68e8af4547bd28545a2e5cc4da61270d
MSKose <saitmustafa1998@gmail.com>
2023-12-27 22:56:14 +0100

    added the 7kyu Reverse the bits in an integer

---
+++ 7 kyu/Reverse the bits in an integer.py
@@ -0,0 +1,13 @@
+'''
+https://www.codewars.com/kata/5959ec605595565f5c00002b
+
+Write a function that reverses the bits in an integer.
+
+For example, the number 417 is 110100001 in binary. Reversing the binary is 100001011 which is 267.
+
+You can assume that the number is not negative.
+'''
+
+# my solution
+def reverse_bits(n):
+    return int((bin(n)[2:])[::-1], 2)
