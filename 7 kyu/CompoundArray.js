7bba52dc0af95487f860c1608e8444891199f42e
MSKose <saitmustafa1998@gmail.com>
2024-01-05 23:46:04 +0100

    added the 7kyu CompoundArray

---
+++ 7 kyu/CompoundArray.js
@@ -0,0 +1,21 @@
+/*
+https://www.codewars.com/kata/56044de2aa75e28875000017
+
+You have to create a method "compoundArray" which should take as input two int arrays of different length and return one int array with numbers of both arrays shuffled one by one.
+
+Input - {1,2,3,4,5,6} and {9,8,7,6} 
+Output - {1,9,2,8,3,7,4,6,5,6}
+*/
+
+// my solution
+function compoundArray(a, b) {
+    let res = [];
+    const max = Math.max(a.length, b.length);
+
+    for (let i = 0; i < max; i++) {
+        if (a[i] != undefined) res.push(a[i]);
+        if (b[i] != undefined) res.push(b[i]);
+    }
+
+    return res;
+}
