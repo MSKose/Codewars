b6bf45e5e17cf8e4ab95eb57d2293ca8429cc3bd
MSKose <saitmustafa1998@gmail.com>
2024-01-07 22:18:02 +0100

    added the 7kyu Averages of numbers

---
+++ 7 kyu/Averages of numbers.py
@@ -0,0 +1,21 @@
+'''
+https://www.codewars.com/kata/57d2807295497e652b000139
+
+#Get the averages of these numbers
+
+Write a method, that gets an array of integer-numbers and return an array of the averages of each integer-number and his follower, if there is one.
+
+Example:
+
+Input:  [ 1, 3, 5, 1, -10]
+Output:  [ 2, 4, 3, -4.5]
+If the array has 0 or 1 values or is null, your method should return an empty array.
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+'''
+
+# my solution
+def averages(arr):
+    if not arr or len(arr) < 2: 
+        return []
+    return [(el + arr[idx + 1]) / 2 for idx, el in enumerate(arr[:-1])]
