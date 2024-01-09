a6ae1826b56d0f9a305e895db44f00c6af49e1d3
MSKose <saitmustafa1998@gmail.com>
2024-01-09 23:00:41 +0100

    added the 7kyu Averages of numbers

---
+++ Java/7kyu/Averages of numbers.java
@@ -0,0 +1,29 @@
+/*
+https://www.codewars.com/kata/57d2807295497e652b000139
+
+#Get the averages of these numbers
+
+Write a method, that gets an array of integer-numbers and return an array of the averages of each integer-number and his follower, if there is one.
+
+Example:
+
+Input:  [ 1, 3, 5, 1, -10]
+Output:  [ 2, 4, 3, -4.5]
+If the array has 0 or 1 values or is null, your method should return an empty array.
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+*/
+
+// my solution
+public class Kata {
+    public static double[] averages(int[] numbers) {
+        if (numbers == null || numbers.length < 2) return new double[0];
+        double[] avgArr = new double[numbers.length - 1];
+    
+        for (int i = 0; i < numbers.length - 1; i++) {
+            avgArr[i] = (double) (numbers[i] + numbers[i + 1]) / 2;
+        }
+    
+        return avgArr;
+    }
+}
