a5b99a58525ad97f348d02894fbf5a77d64396cb
MSKose <saitmustafa1998@gmail.com>
2024-01-27 23:52:17 +0100

    added the 6kyu Make the Deadfish Swim

---
+++ 6 kyu/Make the Deadfish Swim.js
@@ -0,0 +1,30 @@
+/*
+https://www.codewars.com/kata/51e0007c1f9378fa810002a9
+
+Write a simple parser that will parse and run Deadfish.
+
+Deadfish has 4 commands, each 1 character long:
+
+i increments the value (initially 0)
+d decrements the value
+s squares the value
+o outputs the value into the return array
+Invalid characters should be ignored.
+
+parse("iiisdoso")  ==>  [8, 64]
+*/
+
+// my solution
+function parse(data) {
+    let val = 0;
+    let res = [];
+
+    for (let str of data) {
+        if (str === "i") val++;
+        else if (str === "d") val--;
+        else if (str === "s") val *= val;
+        else if (str === "o") res.push(val);
+    }  
+    
+    return res;
+}
