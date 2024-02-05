c7b574b885523f0dfce25f785a0c6461aeb9d492
MSKose <saitmustafa1998@gmail.com>
2024-02-05 23:46:12 +0100

    added the 7kyu Sum of Cubes

---
+++ 7 kyu/Sum of Cubes.py
@@ -0,0 +1,16 @@
+'''
+https://www.codewars.com/kata/59a8570b570190d313000037
+
+Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
+
+Assume that the input n will always be a positive integer.
+
+Examples: (Input --> output)
+
+2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
+3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
+'''
+
+# my solution
+def sum_cubes(n):
+    return sum(i ** 3 for i in range(n + 1))
