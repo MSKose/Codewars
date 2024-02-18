69fb8b0d28d49d6841cb6ffb0a38eec1097fc395
MSKose <saitmustafa1998@gmail.com>
2024-02-18 22:41:25 +0100

    added the 7kyu Area of an arrow

---
+++ 7 kyu/Area of an arrow.py
@@ -0,0 +1,16 @@
+'''
+https://www.codewars.com/kata/589478160c0f8a40870000bc
+
+Area of an arrow
+An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+
+arrow
+
+a and b are integers and > 0
+
+Write a function which returns the area of the arrow.
+'''
+
+# my solution
+def arrow_area(a, b):
+    return a * b / 4
