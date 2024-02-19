ad63462794e3a5c77c49ed1b3f9a7361ba72c07f
MSKose <saitmustafa1998@gmail.com>
2024-02-19 22:39:35 +0100

    added the 7kyu Area of an arrow

---
+++ 7 kyu/Area of an arrow.js
@@ -0,0 +1,15 @@
+/*
+https://www.codewars.com/kata/589478160c0f8a40870000bc
+
+Area of an arrow
+An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+
+arrow
+
+a and b are integers and > 0
+
+Write a function which returns the area of the arrow.
+*/
+
+// my solution
+const arrowArea = (a, b) => a * b / 4;
