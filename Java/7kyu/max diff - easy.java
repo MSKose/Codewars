0d06586278b8b8f82ad5284028c2272dd5c44220
MSKose <saitmustafa1998@gmail.com>
2024-03-10 23:17:02 +0100

    added the 7kyu max diff - easy

---
+++ Java/7kyu/max diff - easy.java
@@ -0,0 +1,24 @@
+/*
+https://www.codewars.com/kata/588a3c3ef0fbc9c8e1000095
+
+You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+lst contains integers, that means it may contain some negative numbers
+if lst is empty or contains a single element, return 0
+lst is not sorted
+[1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+[1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+Have fun!
+*/
+
+// my solution
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+
+public class Kata {
+    public static int maxDiff(int[] lst) {
+        if (lst.length == 0) return 0;
+        IntSummaryStatistics stats = Arrays.stream(lst).summaryStatistics();
+        return stats.getMax() - stats.getMin();
+    }
+}
